e017f57e03b12e5209c27e5bab6337ed906aac1c
mTy8421 <koontyza007@gmail.com>
2022-11-25 21:24:19 +0700

    update icon

--- src/app/set-ui/set-ui.module.ts
+++ src/app/set-ui/set-ui.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatCardModule,
+    MatIconModule,
   ],
   exports: [
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
   ],
 })
 export class SetUIModule { }
